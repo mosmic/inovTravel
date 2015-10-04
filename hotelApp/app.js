6a4b35ce4c54e6d076ab587a3002ae6f79d3a770
Dimitrios Vythoulkas <vythoulkas@hotmail.com>
2015-10-05 01:19:29 +0300

    adding custom directives

--- hotelApp/app.js
+++ hotelApp/app.js
@@ -35,10 +35,51 @@ hotelApp.controller('resultsController', ['$scope', '$resource', function($scope
 }]);
 
 
+// DIRECTIVES
+hotelApp.directive("hotelInfo", function(){
+   return {
+       restrict: 'E',
+       templateUrl: 'directives/hotelInfo.html',
+       replace: true,
+       
+   } 
+});
 
+hotelApp.directive("hotelImages", function(){
+   return {
+       restrict: 'E',
+       templateUrl: 'directives/images.html',
+       replace: true,
+       
+   } 
+});
 
+hotelApp.directive("hotelDescription", function(){
+   return {
+       restrict: 'E',
+       templateUrl: 'directives/hotelDescription.html',
+       replace: true,
+       
+   } 
+});
 
+hotelApp.directive("hotelOffers", function(){
+   return {
+       restrict: 'E',
+       templateUrl: 'directives/hotelOffers.html',
+       replace: true,
+       
+   } 
+});
 
+hotelApp.directive("hotelReviews", function(){
+   return {
+       restrict: 'E',
+       templateUrl: 'directives/hotelReviews.html',
+       replace: true,
+       
+   } 
+});
 
 
 
